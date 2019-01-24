bf86045367c4bec44ab3499d943847346ee606ed
Sumit Munshi <46409920+smunshi@users.noreply.github.com>
2019-01-24 18:09:23 -0500

    add kthread dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fd:
 
 setuptools.setup(
     name="bombfuse",
-    version="0.1",
+    version="0.2",
     author="The Munshi Group",
     author_email="support@munshigroup.com",
     description="Specify a timeout with any given function",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/munshigroup/bombfuse",
     packages=setuptools.find_packages(),
+    install_requires = ['kthread'],
     classifiers=[
         "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
